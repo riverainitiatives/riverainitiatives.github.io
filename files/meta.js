5e731c7911a9771751a3aacc505bfae5a76d1200
Osvaldo D. Rivera, PhD <orivera@ozcorp.io>
2024-01-15 12:51:44 -0400

    first commit

---
+++ files/meta.js
@@ -0,0 +1,74 @@
+(function() {
+	var Realmac = Realmac || {};
+
+	Realmac.meta = {
+		
+		// Set the browser title
+		//
+		// @var String text
+		setTitle: function(text) {
+			return document.title = text;
+		},
+		
+		// Set the content attribute of a meta tag
+		//
+		// @var String name
+		// @var String content
+		setTagContent: function(tag, content){
+			// If the tag being set is title
+			// return the result of setTitle
+			if ( tag === 'title' )
+			{
+				return this.setTitle(content);
+			}
+			
+			// Otherwise try and find the meta tag
+			var tag = this.getTag(tag);
+			
+			// If we have a tag, set the content
+			if ( tag !== false )
+			{
+				return tag.setAttribute('content', content);
+			}
+			
+			return false;
+		},
+		
+		// Find a meta tag
+		//
+		// @var String name
+		getTag: function(name) {
+			var meta = document.querySelectorAll('meta');
+			
+			for ( var i=0; i<meta.length; i++ )
+			{
+				if (meta[i].name == name){
+					return meta[i];
+				}
+			}
+			
+			var tag = document.createElement('meta');
+			tag.name = name;
+			document.getElementsByTagName('head')[0].appendChild(tag);
+			
+			return tag;
+		}
+	};
+ 
+	// Object containing all website meta info
+	var websiteMeta = {"archive-november-2023.html":"Archives for November 2023","category-news---omics.html":"A list of posts in category &ldquo;News - OMICS&rdquo;","category-news.html":"A list of posts in category &ldquo;News&rdquo;","b4cbc1fdbc8bde4401cbb85e9e45e2f7-9.html":"San Juan, Puerto Rico — In a strategic move to propel molecular biology training and innovation to new heights, RIVERA Initiatives Inc. proudly introd","ca18506a475f4cff7e6186a30dfebbbb-8.html":"San Juan, Puerto Rico — In a significant stride towards advancing educational excellence and innovation, RIVERA Initiatives Inc. proudly announces the"};
+ 
+	// pageId must match the key in websiteMeta object
+	var url = window.location.pathname;
+	var pageId = url.substring(url.lastIndexOf('/')+1);
+	if (!pageId || pageId.length == 0){
+		pageId = 'index.html';
+	}
+	pageMeta = websiteMeta[pageId];
+ 
+	// If we have meta for this page
+	if (pageMeta){
+		Realmac.meta.setTagContent('description', pageMeta);
+	}
+ 
+ })();
